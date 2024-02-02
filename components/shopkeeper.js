06f45cec370727e02b3dc43989d97ab8b88f4d99
shashankg361 <shashankg361@gmail.com>
2024-02-02 21:22:09 +0530

    updating shopkeers page

--- components/shopkeeper.js
+++ components/shopkeeper.js
@@ -1,26 +1,23 @@
 import { pool } from "@/pages/_app"
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext, useRef, useState } from "react"
+import { Input } from "postcss";
+import { useContext, useEffect, useRef, useState } from "react"
 
 export default function Shopkeeper(){
     const {userData} = useContext(pool);
 
     const show = ()=>{
         if(userData.Shopkeeper){
-            return <div>working</div>
+            return <RemoveLock />
         }else{
             return <ShowLock lockBtnRef/>
         }
     }
 
-    
-    
-    
     return(
         <div>
             {show()}
-  
         </div>
     )
 };
@@ -29,18 +26,69 @@ export default function Shopkeeper(){
 function ShowLock(){
     const [change , setChange] = useState(false);
     const lockBtnRef = useRef(null);
-    if(window){
-        setInterval(()=>{
-            setChange(!change);
-        },2000);
+    useEffect(()=>{
+
+        const blink = setInterval(()=>{
+                    setChange(!change);
+                    },1000
+        );
+
+        return ()=>clearInterval(blink);            
+    },[change]);
+    
+    return(
+        <div className="text-center mt-10">
+            <FontAwesomeIcon icon={faLock} className="text-5xl text-stone-800"/> 
+            <h1 className="text-xl">To Unlock you have to buy Subscription</h1>
+            <div ref={lockBtnRef} ><button type="button" className={`text-xl w-80 border-2 border-black p-1 rounded-xl ${change && "bg-gray-800 text-white"}`} >Click Here</button></div>
+        </div>
+    )
+}
+
+function RemoveLock(){
+    const [files,setFiles] = useState(null);
+    const [previewUrls,setPreviewUrls] = useState([]);
+    const handleChange = (e)=>{
+        console.log("working");
+        const selectedFiles = Array.from(e.target.files)
+        setFiles(selectedFiles);
+
+        const promises = selectedFiles.map((file) => {
+            return new Promise((resolve) => {
+              const reader = new FileReader();
+              reader.onload = () => resolve(reader.result);
+              reader.readAsDataURL(file);
+            });
+        });
+      
+        Promise.all(promises).then((urls) => setPreviewUrls(urls));
+
     }
     
 
-    return(
-        <div className="text-center mt-10">
-            <FontAwesomeIcon icon={faLock} className="text-5xl text-stone-800"/>
-            <h1 className="text-xl">To Unlock you have to buy Subscription</h1>
-            <div ref={lockBtnRef} className={`text-xl border-2 border-black p-1 rounded-xl ${change ? "bg-gray-800 text-white":""}`}><button type="button"  >Click Here</button></div>
+    const handleSubmit = ()=>{
+        console.log("working");
+        if(!files){
+            alert("please enter file");
+        };
+        console.log(files);
+    }
+
+    return<div>
+        <div>
+            <input type="file" onChange={handleChange} multiple/>
+            <input type="submit"  onClick={handleSubmit} />
         </div>
-    )
+        <div >
+        {previewUrls.length > 0 && (
+        <div className="flex">
+          <p>Previews:</p>
+          {previewUrls.map((url, index) => (
+            <img key={index} src={url} alt={`Preview ${index}`} style={{ maxWidth: '20%' }} />
+          ))}
+        </div>
+      )}
+        </div>
+
+    </div>
 }
