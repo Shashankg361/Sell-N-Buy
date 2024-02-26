d006c44fceeb6d03abf2320e68add42c200f53aa
shashankg361 <shashankg361@gmail.com>
2024-02-26 10:34:49 +0530

    Booking endpoin

--- pages/api/Booking.js
+++ pages/api/Booking.js
@@ -7,16 +7,26 @@ const bcrypt = require('bcrypt');
 export default async function BookingEndPoint(req,res){
     if(req.method === 'POST'){
         const {userName,userEmail,productId,productName,shopName} = req.body;
+        
+        const BookingTime = Date.now();
         const db = client.db('MobileDets');
-        const collection = db.collection('Booked');
+        const BookingCollection = db.collection('Booking');
+        const DetailsCollection = db.collection('Details');
         const dataStore = {userName,userEmail,productId,productName,shopName};
+        console.log("stored",dataStore);
         const sceretkey = crypto.randomBytes(32).toString('hex');
         const token = jwt.sign({dataStore},sceretkey,{expiresIn:'24h'});
+        const data = {userName,userEmail,productId,productName,shopName,token,BookingTime}
+        console.log("working");
         try{
-            const response = await collection.insertOne(dataStore);
-            res.status(200).json({message:"Booked!"});
+            const response = await BookingCollection.insertOne(data);
+            const query = {_id:`${productId}`};
+            const updateBooked = await DetailsCollection.updateOne(query,{$set:{Booked:true}});
+            console.log("Update",updateBooked);
+            console.log("Booking response",response);
+            res.status(200).json({message:"Booked!",Booked:true});
         }catch(error){
-            res.status(200).json({message:error});
+            res.status(200).json({message:`Internal sever error ${error}`,Booked:false});
         }
         
     }
--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -7,7 +7,8 @@ import { faIndianRupee } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 
 export default function Product(){
-    const {mobileDets,userData} = useContext(pool);
+    const {mobileDets,userData,LoggedIn} = useContext(pool);
+    
     
     const router = useRouter();
     const query = router.query.productId;
@@ -15,6 +16,7 @@ export default function Product(){
         return mobile._id == query
     })
     const [currentImage,setCurrentImage]=useState(product && product[0]?.ImagesUrl[0]);
+    const [Booked,setBooked] = useState(product && product[0]?.Booked);
     const images = product[0].ImagesUrl;
     console.log("Images",images);
     useEffect(()=>{
@@ -23,16 +25,25 @@ export default function Product(){
 
     //function for booking the product
     const Booking = async ()=>{
-        const userName = userData.Name;
-        const userEmail = userData.Email;
-        const productId = product[0]?._id;
-        const productName = product[0]?.Mname;
-        const shopName = product[0]?.ShopName;
+        if(LoggedIn){
+            const userName = userData.Name;
+            const userEmail = userData.Email;
+            const productId = product && product[0]?._id;
+            const productName = product[0]?.Mname;
+            const shopName = product[0]?.ShopName;
 
-        const data = {userName,userEmail,productId,productName,shopName};
-        const response = await axios.post('/api/Booking',{data});
-        const message = response.data;
-        alert(message.message);
+            const data = {userName,userEmail,productId,productName,shopName};
+            console.log("datatata",data);
+            const response = await axios.post('/api/Booking',data);
+            const {message,Booked} = response.data;
+            console.log("Response",message)
+            setBooked(Booked);
+
+            alert(message);
+        }else{
+            alert("Please LogIn!")
+        }
+        
 
     }
     
@@ -59,7 +70,7 @@ export default function Product(){
                     <div className="flex flex-col text-2xl shadow-lg shadow-gray-900 rounded-lg p-2 w-3/5 m-1 p-4"> 
                         <div className="flex justify-between">
                             <h1 className="font-normal text-2xl ml-3 underline decoration-double decoration-gray-900 decoration-2" >{product[0].Mname}{`(${product[0].Color})`}</h1>
-                            <div className={`border-2 ${product[0].Booked ? "hover:bg-red-300":"hover:bg-green-300"} border-none ${product[0].Booked ? "bg-red-500":"bg-green-500"} rounded-lg p-2`}><button onClick={Booking}>{ product[0].Booked ? "Booked" : "Click to Book"}</button></div>
+                            <div className={`border-2 ${Booked ? "hover:bg-red-300":"hover:bg-green-300"} border-none ${Booked ? "bg-red-500":"bg-green-500"} rounded-lg p-2`}><button onClick={Booking}>{ product[0].Booked ? "Booked" : "Click to Book"}</button></div>
                         </div>
                     
                     <h1 className="font-bold text-4xl antialiased italic m-5"><FontAwesomeIcon icon={faIndianRupee} />{product[0]?.Price}</h1>
